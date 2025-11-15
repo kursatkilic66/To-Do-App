5d39bb30d428592572aa0f0dfc18be74d4fb378a
Kursat Kilicarslan <kursatkilicarslan66@gmail.com>
2025-11-15 11:38:45 +0300

    fix

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -11,7 +11,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <MainPage></MainPage>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GreetingPage />} />
